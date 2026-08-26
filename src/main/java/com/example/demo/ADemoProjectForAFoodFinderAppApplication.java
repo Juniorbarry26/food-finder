01d26ea4ea4a836203af9661ce75816610b5609f
Juniorbarry26 <ab22316053@utg.edu.gm>
2026-08-26 16:02:30 +0000

    project initializations

---
+++ src/main/java/com/example/demo/ADemoProjectForAFoodFinderAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ADemoProjectForAFoodFinderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ADemoProjectForAFoodFinderAppApplication.class, args);
+	}
+
+}
